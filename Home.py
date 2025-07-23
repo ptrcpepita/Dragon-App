799baeea9e803d4524876a195694130f55b5e3b1
ptrcpepita <work.patriciapepita@gmail.com>
2025-07-23 14:21:47 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.markdown(
         """
         <style>
             [data-testid = "stSidebarNav"]{
-            background-image: url("https://raw.githubusercontent.com/ptrcpepita/Dragon-App/74d7b0924a521afba224fca618d9e0771ea525e2/dragon_logo_png.png");
+            background-image: url("https://raw.githubusercontent.com/ptrcpepita/Dragon-App/asset/74d7b0924a521afba224fca618d9e0771ea525e2/dragon_logo_png.png");
             background-repeat: no-repeat;
             background-size: 210px;
             padding-top: 100px;
@@ -19,8 +19,8 @@ st.markdown(
 st.markdown(
     """
     <div style="margin-left: -7px;">
-        <img src="https://raw.githubusercontent.com/ptrcpepita/Dragon-App/asset/dragon2_logo_png.png" width="500">
-    </div>
+        <img src="https://raw.githubusercontent.com/ptrcpepita/Dragon-App/asset/ef59d7794c3f32d47a9926637facc2a66380f725/dragon2_logo_png.png" width="500">
+        </div>
     """,
     unsafe_allow_html=True
 )
