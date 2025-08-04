d11a49f7ee82b0d2a0b24d7cda2dad2220fd12b7
ptrcpepita <work.patriciapepita@gmail.com>
2025-08-04 10:29:49 +0700

    Update 2Drop Duplicates.py

--- pages/2Drop Duplicates.py
+++ pages/2Drop Duplicates.py
@@ -16,28 +16,6 @@ st.subheader("📂 Insert an Excel File Link")
 
 url = st.text_input("Paste the one drive public Excel file URL here (format = one drive link + '&download=1'):")
 
-#if url:
-   # try:
-       # response = requests.get(url)
-       # response.raise_for_status()  # Raise error for bad status
-       # df = pd.read_excel(BytesIO(response.content))  # For .xlsx files
-       # st.success("File loaded successfully!")
-       # st.dataframe(df.head())  
-  #  except Exception as e:
-      #  st.error(f"Failed to load the Excel file: {e}")
-
-#uploaded_file = st.file_uploader('Upload here', type=["xlsx", "csv"])
-
-#current_file_name = uploaded_file.name if uploaded_file else None
-#if ("uploaded_file_name" in st.session_state # cek apakah filenya berubah/ilang
-   # and st.session_state.uploaded_file_name != current_file_name):
-    #for key in ["original_df", "df", "custom_dtypes", "change_history", "change_history2"]:
-       # st.session_state.pop(key, None)
-   # st.session_state.uploaded_file_name = current_file_name
-    
-# elif uploaded_file and "uploaded_file_name" not in st.session_state:
-   # st.session_state.uploaded_file_name = current_file_name
-
 current_file_name = os.path.basename(url) if url else None
 if ("url_name" in st.session_state # cek apakah filenya berubah/ilang
     and st.session_state.url_name != current_file_name):
@@ -48,9 +26,6 @@ if ("url_name" in st.session_state # cek apakah filenya berubah/ilang
 elif url and "url_name" not in st.session_state:
     st.session_state.uploaded_file_name = current_file_name
         
-#if uploaded_file:
-    #try:
-        #if "original_df" not in st.session_state:
 if url:
     try:
         if "original_df" not in st.session_state:
@@ -63,80 +38,80 @@ if url:
             st.success("File loaded successfully!")
             st.write("Data Preview:")
             st.dataframe(df.head(10))  
-
+        
             # REMOVE DUPLICATE
-            # indent: 3
-            st.markdown("")
-            if st.button('Remove Duplicate ⏭️', key='dupli'):
-                st.session_state['removedupli_clicked'] = True
+            # indent: 2
+        st.markdown("")
+        if st.button('Remove Duplicate ⏭️', key='dupli'):
+            st.session_state['removedupli_clicked'] = True
 
-            if st.session_state['removedupli_clicked']:
-                st.markdown("---")
-                st.subheader('📑 Remove Duplicate')
-                st.write("Data shape: ", {len(df)})
-                input_dupli = st.number_input("How many base column to remove duplicate?", min_value=0, max_value =len(df.columns), step=1, format='%d', key='num_dupli')
-                st.write("min: 1, max:", len(df.columns))
+        if st.session_state['removedupli_clicked']:
+            st.markdown("---")
+            st.subheader('📑 Remove Duplicate')
+            st.write("Data shape: ", {len(df)})
+            input_dupli = st.number_input("How many base column to remove duplicate?", min_value=0, max_value =len(df.columns), step=1, format='%d', key='num_dupli')
+            st.write("min: 1, max:", len(df.columns))
 
                 #if input_dupli == 0:
                     #st.info("No duplicate will be removed.")
                 #else:
                     #if st.button("Ok", key='ok'):
-                if input_dupli != 0:
-                    st.session_state['dupli_config'] = True
+            if input_dupli != 0:
+                st.session_state['dupli_config'] = True
 
-                    if st.session_state.get('dupli_config', False):
-                        selected_cols = []
-                        for i in range(st.session_state['num_dupli']):
-                            col = st.selectbox(f"Choose column {i+1}", options=[""] + list(df.columns), key=f"dupli_col_{i}")
-                            st.dataframe(df[col].value_counts())
-                            #st.write("Total of unique value: ",df[col].nunique())
-                            if col:
-                                selected_cols.append(col)
+                if st.session_state.get('dupli_config', False):
+                    selected_cols = []
+                    for i in range(st.session_state['num_dupli']):
+                        col = st.selectbox(f"Choose column {i+1}", options=[""] + list(df.columns), key=f"dupli_col_{i}")
+                        st.dataframe(df[col].value_counts())
+                        #st.write("Total of unique value: ",df[col].nunique())
+                        if col:
+                            selected_cols.append(col)
                             
-                        dupli_row = df[df.duplicated(subset=selected_cols, keep=False)]
-                        dupli_row = dupli_row.sort_values(by=selected_cols)
-                        st.write("Duplicated data rows:")
-                        st.dataframe(dupli_row)
+                    dupli_row = df[df.duplicated(subset=selected_cols, keep=False)]
+                    dupli_row = dupli_row.sort_values(by=selected_cols)
+                    st.write("Duplicated data rows:")
+                    st.dataframe(dupli_row)
         
-                        if selected_cols:
-                            keep_option = st.radio("Which duplicate data should be DROP?", options=["First", "Last", "All", "Choose index"], index=0, horizontal=True)
-                            st.markdown("""
-                                Note:
-                                        - First: drop the first, keep the last occurrence
-                                        - Last: drop keep last, keep the first occurrence
-                                        - All: drop all duplicate
-                                        - Choose index: choose index you want to DROP
-                            """)
+                    if selected_cols:
+                        keep_option = st.radio("Which duplicate data should be DROP?", options=["First", "Last", "All", "Choose index"], index=0, horizontal=True)
+                        st.markdown("""
+                        Note:
+                        - First: drop the first, keep the last occurrence
+                        - Last: drop keep last, keep the first occurrence
+                        - All: drop all duplicate
+                        - Choose index: choose index you want to DROP
+                        """)
                                 
-                            choose_index = False
-                            if keep_option == "Choose index":
-                                choose_index = True
-                                index = st.text_input("Enter index number you want to **DROP** (separated by commas): ", placeholder='eg. 9, 15, 180')
-                                if index:
+                        choose_index = False
+                        if keep_option == "Choose index":
+                            choose_index = True
+                            index = st.text_input("Enter index number you want to **DROP** (separated by commas): ", placeholder='eg. 9, 15, 180')
+                            if index:
                                     #try:
-                                    number = [int(x.strip()) for x in index.split(',') if x.strip().isdigit()]
-                                    st.write(f"Index number entered: {number}")
+                                number = [int(x.strip()) for x in index.split(',') if x.strip().isdigit()]
+                                st.write(f"Index number entered: {number}")
                                         #except Exception as e:
                                             #st.error(f"Invalid input: {e}")
                     
-                        if st.button("Remove", key='remove dupli'):
+                    if st.button("Remove", key='remove dupli'):
                             #df_cleaned = None
-                            if keep_option == "Last":
-                                df = df.drop_duplicates(subset=selected_cols, keep='first').reset_index(drop=True)
-                            elif keep_option == "First":
-                                df = df.drop_duplicates(subset=selected_cols, keep='last').reset_index(drop=True)
-                            elif keep_option == "All":
-                                dup_mask = df.duplicated(subset=selected_cols, keep=False)
-                                df = df[~dup_mask].reset_index(drop=True)
-                            elif choose_index == True:
-                                df = df.drop(number).reset_index(drop=True)
-                                choose_index = False
+                        if keep_option == "Last":
+                            df = df.drop_duplicates(subset=selected_cols, keep='first').reset_index(drop=True)
+                        elif keep_option == "First":
+                            df = df.drop_duplicates(subset=selected_cols, keep='last').reset_index(drop=True)
+                        elif keep_option == "All":
+                            dup_mask = df.duplicated(subset=selected_cols, keep=False)
+                            df = df[~dup_mask].reset_index(drop=True)
+                        elif choose_index == True:
+                            df = df.drop(number).reset_index(drop=True)
+                            choose_index = False
                         
-                            st.success(f"Successfully remove duplicate based on {', '.join(selected_cols)} using '{keep_option}'")
-                            st.write("New data:")
-                            st.dataframe(df.head())
-                            st.write("New data shape: ",len(df))
-                            st.session_state.df = df
+                        st.success(f"Successfully remove duplicate based on {', '.join(selected_cols)} using '{keep_option}'")
+                        st.write("New data:")
+                        st.dataframe(df.head())
+                        st.write("New data shape: ",len(df))
+                        st.session_state.df = df
 
             st.write("")
             st.markdown("")
